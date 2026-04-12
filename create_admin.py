dd24c9b50640454208c6001cb0a2988b878d750a
jinsm01 <jinsm01@gmail.com>
2026-04-12 18:47:55 +0800

    chore: clean up temporary scripts and files

--- create_admin.py
+++
@@ -1,7 +0,0 @@
-from apps.users.models import User
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@testhub.com', 'admin123')
-    print('Superuser admin created with password admin123')
-else:
-    print('Superuser admin already exists')
